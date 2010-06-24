896176755c1b6aa1995f1dede23b6cf2780f82a8
fbachmann <fbachmann@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-06-24 20:43:40 +0000

    added flexform dummy
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/feusermanagement/trunk@34883 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -2,8 +2,10 @@
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
 t3lib_div::loadTCA('tt_content');
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key';
-
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key,pages';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1']=
+  'pi_flexform';
+ t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY.'/flexform_ds.xml');
 
 t3lib_extMgm::addPlugin(array('LLL:EXT:feusermanagement/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
 
